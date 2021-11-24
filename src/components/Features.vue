86eb1000e633f0f395bdd33f9e766ac69709d9c4
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-24 10:54:33 +0100

    v-for for data features

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="features">
     <div class="container">
-      <ul class="list-features flex">
+      <ul class="list-features flex no-list-style">
         <li v-for="(feature, index) in features" :key="`feature-${index}`">
           <img :src="feature.imgFeatures" :alt="feature.text" />
-          <span></span>
+          <span>feature.text</span>
         </li>
       </ul>
     </div>
@@ -18,27 +18,27 @@ export default {
     return {
       features: [
         {
-          imgFeatures: require('@/assets/img/buy-comics-digital-comics.png'),
+          imgFeatures: require("@/assets/img/buy-comics-digital-comics.png"),
           text: "Digital comics",
         },
         {
-          imgFeatures: require('@/assets/img/buy-comics-merchandise.png'),
+          imgFeatures: require("@/assets/img/buy-comics-merchandise.png"),
           text: "DC Merchandise",
         },
         {
-          imgFeatures: require('@/assets/img/buy-comics-subscriptions.png'),
+          imgFeatures: require("@/assets/img/buy-comics-subscriptions.png"),
           text: "Subscription",
         },
         {
-          imgFeatures: require('@/assets/img/buy-comics-shop-locator.png'),
+          imgFeatures: require("@/assets/img/buy-comics-shop-locator.png"),
           text: "Comic shop locator",
         },
         {
-          imgFeatures: require('@/assets/img/buy-dc-power-visa.svg'),
+          imgFeatures: require("@/assets/img/buy-dc-power-visa.svg"),
           text: "DC Power Visa",
         },
       ],
-    },
+    };
   },
 };
 </script>
