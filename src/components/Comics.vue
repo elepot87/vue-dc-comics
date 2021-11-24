27935100eaba470a751c83fee6585b9b25c87b3c
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-11-24 16:59:06 +0100

    add v-for for comics

---
+++ src/components/Comics.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img :src="thumb" :alt="series" />
+    <div class="title-comics">
+      {{ series }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Comics",
+  props: {
+    thumb: String,
+    series: String,
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
